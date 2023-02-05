8de4ec0060f633ed39c0246726d7bcc5e72678a7
Awann <101172637+yaboidimsum@users.noreply.github.com>
2023-02-05 15:20:59 +0700

    Add files via upload

---
+++ DAY 22-30/(24) Merging names to emails/Mail Merge Project Start/main.py
@@ -0,0 +1,23 @@
+#TODO: Create a letter using starting_letter.txt
+PLACEHOLDER = "[name]"
+#for each name in invited_names.txt
+#Replace the [name] placeholder with the actual name.
+#Save the letters in the folder "ReadyToSend".
+    
+#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+with open("./Input/Names/invited_names.txt") as names_file:
+    list_name = names_file.readlines()
+
+with open("./Input/Letters/starting_letter.txt") as letter_file:
+        letter_contents = letter_file.read()
+        for name in list_name:
+            strip_name=name.strip()
+            new_letter = letter_contents.replace(PLACEHOLDER, strip_name)
+            with open(f"./Output/ReadyToSend/letter_for_{strip_name}.txt", mode="w") as completed_letter:
+                completed_letter.write(new_letter)
+
+
+#Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+
+
+#Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
