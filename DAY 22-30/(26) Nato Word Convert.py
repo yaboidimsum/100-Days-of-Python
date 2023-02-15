27fbacac24293eac509995bdd1b04874194be2a5
Awann <101172637+yaboidimsum@users.noreply.github.com>
2023-02-15 18:19:17 +0700

    Create (26) Nato Word Convert.py

---
+++ DAY 22-30/(26) Nato Word Convert.py
@@ -0,0 +1,11 @@
+#TODO 1. Create a dictionary in this format:
+nato_words = pd.read_csv("nato_phonetic_alphabet.csv")
+
+phonetic_dict = {row.letter:row.code for (index, row) in nato_words.iterrows()}
+# print(phonetic_dict)
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+word = input("Enter a word: ").upper()
+output = [phonetic_dict[letter] for letter in word]
+
+print(output)
